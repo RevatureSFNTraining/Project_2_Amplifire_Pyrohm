cdb0d44aef4fd8b9882b1d750bf6bb762b76f6b2
Mfernan4 <mfernandez000005@gmail.com>
2021-10-04 22:43:25 +0000

    Added my stuff

---
+++ force-app/main/default/aura/IngCard/IngCardController.js
@@ -0,0 +1,15 @@
+({
+    cardEventA1 : function(cmp, event, helper) { 
+        var cmpEvent = cmp.getEvent("CardEventRegister"); 
+        cmpEvent.setParams({"cardContent" : cmp.get("v.ing")});
+        cmpEvent.setParams({"type" : "A1"});
+        cmpEvent.fire(); 
+    },
+    
+    cardEventA2 : function(cmp, event, helper) { 
+        var cmpEvent = cmp.getEvent("CardEventRegister"); 
+        cmpEvent.setParams({"cardContent" : cmp.get("v.ing")});
+        cmpEvent.setParams({"type" : "A2"});
+        cmpEvent.fire(); 
+    }
+})
